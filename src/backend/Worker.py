858166a113c6172ac397e9de4979d642a20462a9
Wallen-Ribeiro <up202109260@g.uporto.pt>
2024-12-10 20:55:27 +0000

    started worker health thread

--- src/backend/Worker.py
+++ src/backend/Worker.py
@@ -8,11 +8,11 @@ global_counter_list = {}
 known_workers = set()
 
 def check_lists_in_global_counter(ident):
-    with open(f"server/server_list_{ident}.json",'r') as file:
+    with open(f"server/server_list_{ident}.json", 'r') as file:
         shopping_lists = json.load(file)
 
-    for shopping_list in shopping_lists: 
-        if shopping_list["id"] not in global_counter_list: 
+    for shopping_list in shopping_lists:
+        if shopping_list["id"] not in global_counter_list:
             global_counter_list[shopping_list["id"]] = GlobalCounter(shopping_list["id"], shopping_list)
             existing_data = read_list(shopping_list["id"], ident)
             global_counter_list[shopping_list["id"]].list["items"] = existing_data["items"]
@@ -22,110 +22,124 @@ def read_list(id, ident):
     json_file = f"server/server_list_{ident}.json"
     with open(json_file, 'r') as file:
         shopping_lists = json.load(file)
-    
-    shopping_list = {}
+
     for shopping_list in shopping_lists:
-        if shopping_list['id'] == id: 
+        if shopping_list['id'] == id:
             return shopping_list
 
 def worker_task(ident):
     context = zmq.Context()
+
     socket = context.socket(zmq.REQ)
     socket.identity = u"Worker-{}".format(ident).encode("ascii")
     socket.connect("ipc://backend.ipc")
-    known_workers = set()
 
+    health_socket = context.socket(zmq.REP)
+    health_socket.bind(f"ipc://Worker-{ident}.ipc")
+
+    known_workers = set()
     socket.send(b"READY")
 
     print(f"Worker-{ident} started and connected to backend.")
 
     while True:
-        # Receive the request from the backend
-        message = socket.recv_multipart()
-        
-        if len(message) < 3:
-            print(f"Worker-{ident} received an unexpected message format: {message}")
+        poller = zmq.Poller()
+        poller.register(socket, zmq.POLLIN)
+        poller.register(health_socket, zmq.POLLIN)
+
+        events = dict(poller.poll())
+
+        # Handle health-check requests
+        if health_socket in events:
+            message = health_socket.recv()
+            if message == b"PING":
+                health_socket.send(b"PONG")
             continue
-        
-        address, empty, request = message
-        print(f"Worker-{ident} received request: {request.decode()}")
 
-        # Process the request
-        try:
-            request_data = json.loads(request.decode("utf-8"))
-            action = request_data.get("action")
-            list = request_data.get("list")
-            crdt_states = request_data.get("crdt_states")
+        # Handle normal backend requests
+        if socket in events:
+            message = socket.recv_multipart()
 
-            if action == "update_workers":
-                new_workers = set(request_data.get("workers", []))
-                if new_workers != known_workers:
-                    known_workers = new_workers
-                    print(f"Worker-{ident} knows the workers: {known_workers}")
-                response = {"status": "success"}
+            if len(message) < 3:
+                print(f"Worker-{ident} received an unexpected message format: {message}")
+                continue
 
-            elif action == "get_list":
-                with open(f"server/server_list_{ident}.json", "r") as file:
-                    lists = json.load(file)
-                for list_aux in lists:
-                    if int(list_aux["id"]) == int(request_data.get("list_id")):
-                        list = list_aux
-                        break
-                response = {"status": "success", "list": list}
-            elif action == "update_list":
-                with open(f"server/server_list_{ident}.json", "r") as file: 
-                    lists = json.load(file)
+            address, empty, request = message
+            print(f"Worker-{ident} received request: {request.decode()}")
 
-                check_lists_in_global_counter(ident)
-                global_counter_list[list["id"]].list, global_counter_list[list["id"]].crdt_states = global_counter_list[list["id"]].merge_version(list, crdt_states)
-                print(f"The new updated list is {global_counter_list[list['id']].list}")
-                for cart in lists: 
-                    if int(cart["id"]) == int(list["id"]):
-                        print(f"Found")
-                        cart["items"] = global_counter_list[list["id"]].list["items"]
-                        cart["crdt_states"] = global_counter_list[list["id"]].crdt_states
-                        break
-                print(f"Updating list: {global_counter_list[list['id']].list['items']}")
-                with open(f"server/server_list_{ident}.json", "w") as file:
-                    json.dump(lists, file, indent=4)
-                
-                global_counter_dict = global_counter_list[list["id"]].to_dict()
-                response = {"status": "success", "list": global_counter_dict["list"], "crdt_states": global_counter_dict["crdt_states"]}
-            elif action == "create_list": 
-                with open(f"server/server_list_{ident}.json", "r") as file:
-                    existing_lists = json.load(file)
-                
-                if isinstance(existing_lists, dict): 
-                    existing_lists  =json.load(file)
-                
-                existing_lists.append(list)
-                with open(f"server/server_list_{ident}.json", "w") as file:
-                    json.dump(existing_lists, file, indent=4)
+            # Process the request
+            try:
+                request_data = json.loads(request.decode("utf-8"))
+                action = request_data.get("action")
+                list = request_data.get("list")
+                crdt_states = request_data.get("crdt_states")
 
-                response = {"status" :"success", "message": f"List created: {list['items']}"}   
-            
-            elif action =="delete_list": 
-                with open(f"server/server_list_{ident}.json", "r") as file: 
-                    existing_lists = json.load(file)
-                
-                for list_aux in existing_lists: 
-                    if int(list_aux["id"]) == int(list["id"]): 
-                        print(f"Deleting list: {list['id']}")
-                        existing_lists.remove(list_aux)
-                        break
-            
-                with open(f"server/server_list_{ident}.json", "w") as file:
-                    json.dump(existing_lists, file, indent=4)
+                if action == "update_workers":
+                    new_workers = set(request_data.get("workers", []))
+                    if new_workers != known_workers:
+                        known_workers = new_workers
+                        print(f"Worker-{ident} knows the workers: {known_workers}")
+                    response = {"status": "success"}
 
-                response = {"status": "success", "message": f"List deleted: {list['id']}"}
+                elif action == "get_list":
+                    with open(f"server/server_list_{ident}.json", "r") as file:
+                        lists = json.load(file)
+                    for list_aux in lists:
+                        if int(list_aux["id"]) == int(request_data.get("list_id")):
+                            list = list_aux
+                            break
+                    response = {"status": "success", "list": list}
+                elif action == "update_list":
+                    with open(f"server/server_list_{ident}.json", "r") as file:
+                        lists = json.load(file)
 
-            else:
-                response = {"status": "error", "message": "Invalid action"}
-        except Exception as e:
-            response = {"status": "error", "message": f"Failed to process request: {e} on action {action}"}
+                    check_lists_in_global_counter(ident)
+                    global_counter_list[list["id"]].list, global_counter_list[list["id"]].crdt_states = global_counter_list[list["id"]].merge_version(list, crdt_states)
+                    print(f"The new updated list is {global_counter_list[list['id']].list}")
+                    for cart in lists:
+                        if int(cart["id"]) == int(list["id"]):
+                            print(f"Found")
+                            cart["items"] = global_counter_list[list["id"]].list["items"]
+                            cart["crdt_states"] = global_counter_list[list["id"]].crdt_states
+                            break
+                    print(f"Updating list: {global_counter_list[list['id']].list['items']}")
+                    with open(f"server/server_list_{ident}.json", "w") as file:
+                        json.dump(lists, file, indent=4)
 
-        # Send response back to the backend
-        socket.send_multipart([address, b"", json.dumps(response).encode("utf-8")])
+                    global_counter_dict = global_counter_list[list["id"]].to_dict()
+                    response = {"status": "success", "list": global_counter_dict["list"], "crdt_states": global_counter_dict["crdt_states"]}
+                elif action == "create_list":
+                    with open(f"server/server_list_{ident}.json", "r") as file:
+                        existing_lists = json.load(file)
+
+                    existing_lists.append(list)
+                    with open(f"server/server_list_{ident}.json", "w") as file:
+                        json.dump(existing_lists, file, indent=4)
+
+                    response = {"status": "success", "message": f"List created: {list['items']}"}
+
+                elif action == "delete_list":
+                    with open(f"server/server_list_{ident}.json", "r") as file:
+                        existing_lists = json.load(file)
+
+                    for list_aux in existing_lists:
+                        if int(list_aux["id"]) == int(list["id"]):
+                            print(f"Deleting list: {list['id']}")
+                            existing_lists.remove(list_aux)
+                            break
+
+                    with open(f"server/server_list_{ident}.json", "w") as file:
+                        json.dump(existing_lists, file, indent=4)
+
+                    response = {"status": "success", "message": f"List deleted: {list['id']}"}
+
+                else:
+                    response = {"status": "error", "message": "Invalid action"}
+            except Exception as e:
+                response = {"status": "error", "message": f"Failed to process request: {e} on action {action}"}
+
+            # Send response back to the backend
+            socket.send_multipart([address, b"", json.dumps(response).encode("utf-8")])
 
 if __name__ == '__main__':
     ident = sys.argv[1]
--- src/backend/loadbalancer.py
+++ src/backend/loadbalancer.py
@@ -1,30 +1,62 @@
 from __future__ import print_function
-import multiprocessing
+import threading
 import zmq
 import Worker
-import json 
-import Client
+import json
 from HashRing import HashRing
+import time
 
-clients_lists = {}
+def check_worker_health(context, workers, removed_workers):
+    while True:
+        for worker in list(workers):  
+            try:
+                health_socket = context.socket(zmq.REQ)
+                health_socket.linger = 0
+                health_socket.connect(f"ipc://{worker}.ipc")
+                
+                health_socket.send(b"PING")
+                
+                if not health_socket.poll(timeout=1000):  # 1-second timeout
+                    raise Exception(f"Worker {worker} did not respond.")
+
+                reply = health_socket.recv()
+                if reply != b"PONG":
+                    raise Exception(f"Unexpected response from {worker}: {reply}")
+            
+            except Exception as e:
+                print(f"Worker {worker} is down: {e}")
+                workers.remove(worker)  
+                removed_workers.add(worker) 
+
+
+            finally:
+                health_socket.close()
+
+        time.sleep(5)  
 
 def main():
     count = 2
     backend_ready = False
     workers = set()
-    previous_workers = set()  # Track the previous state of the worker list
+    removed_workers = set()
+    previous_workers = set()  
     ring = HashRing()
     context = zmq.Context()
-    frontend = context.socket(zmq.ROUTER) 
+    frontend = context.socket(zmq.ROUTER)
     frontend.bind("ipc://frontend.ipc")
-    
-    backend = context.socket(zmq.ROUTER) 
+
+    backend = context.socket(zmq.ROUTER)
     backend.bind("ipc://backend.ipc")
 
-    # Poller for load balancer
+    # worker life checker thread
+    health_checker = threading.Thread(
+        target=check_worker_health, args=(context, workers, removed_workers), daemon=True
+    )
+    health_checker.start()
+
     poller = zmq.Poller()
-    poller.register(frontend, zmq.POLLIN)  # Monitor frontend for client requests
-    poller.register(backend, zmq.POLLIN)  # Monitor backend for worker responses
+    poller.register(frontend, zmq.POLLIN)  
+    poller.register(backend, zmq.POLLIN)  
 
     print("Broker is ready and polling...")
 
@@ -44,17 +76,19 @@ def main():
             workers.add(worker.decode("utf-8"))
             ring = HashRing(workers)
             print(f"Workers {workers}")
+
             if workers and not backend_ready:
                 poller.register(frontend, zmq.POLLIN)
                 backend_ready = True
+
             if client != b"READY" and len(request) > 3:
                 empty, reply = request[3:]
                 frontend.send_multipart([client, b"", reply])
 
-            # workers list updated
+            # Workers list updated
             if workers != previous_workers:
                 print("Worker list updated. Sending updates...")
-                previous_workers = workers.copy()  
+                previous_workers = workers.copy()
                 worker_list_message = json.dumps({"action": "update_workers", "workers": list(workers)})
                 for worker in workers:
                     backend.send_multipart([worker.encode("utf-8"), b"", b"", b"", worker_list_message.encode("utf-8")])
@@ -68,14 +102,13 @@ def main():
             list_id = request_data.get("list_id", "default_key")
             assigned_workers = ring.get_preference_list(str(list_id))
 
-            for assigned_worker in assigned_workers: 
+            for assigned_worker in assigned_workers:
                 print(f"Assigned worker: {assigned_worker} for list ID: {list_id}")
                 backend.send_multipart([assigned_worker.encode("utf-8"), b"", client, b"", request])
-            
+
             if not workers:
                 poller.unregister(frontend)
                 backend_ready = False
-
 
 if __name__ == '__main__':
     main()
